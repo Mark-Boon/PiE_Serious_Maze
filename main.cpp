58af46eab45bc832683459c36ce21d257f99edd5
DoveDoof <lennartknoll@live.nl>
2017-09-04 16:19:26 +0200

    main.cpp
    baseline voor een main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include "read.h"
 #include "player.h"
 #include "draw.h"
 
@@ -7,18 +8,20 @@
 
 
 int main() {
+	Level lvl1("lvl1.txt");
+	Level* lvl1p = &lvl1;
 	
 	Player player(1,1,7);
-	int size = player.fov*2+1;
+	int size = player.los*2+1;
 	
-	std::vector<int> matrix(size*size, 1);
 	
 	Window canvas;
 	canvas.resize(size, size);
 	
 	int i=1;
 	while(!GetAsyncKeyState(VK_ESCAPE)){
-		canvas.update(size, matrix);
+		player.update_los_grid( lvl1p );
+		canvas.update(size, player.los_grid);
 		Sleep(100);
 	}
 	
