810b549d4f637a91a5c5cb214eb3a0ea3b007560
DoveDoof <lennartknoll@live.nl>
2017-09-06 23:26:01 +0200

    Plaatst op 1/10 van beschikbare vakjes een random getal tussen 1-9

--- read.cpp
+++ read.cpp
@@ -2,25 +2,34 @@
 
 Level::Level(std::string filename){
 // First part of the constructor: determine the dimensions of the level.
-	this->filename = filename;				// Every object in 'Level' stores its given filename in member variable 'filename'.
+	// Every object in 'Level' stores its given filename in member variable 'filename'.
+	this->filename = filename;
 	std::fstream file_maze;
 	file_maze.open(filename);
-	if (file_maze.is_open()){				// First check whether the file is opened.
+	// First check whether the file is opened.
+	if (file_maze.is_open()){
 		char n;
 		int width=0;
 		int height=1;
-		while (file_maze>>n){				// Read through entire file.
-			width++;	
-			if (file_maze.peek()=='\n')		// If we encounter \n, there must be a new line so height +1.
+		// Read through entire file.
+		while (file_maze>>n){
+			width++;
+			// Keep track of number of free spaces in maze
+			if (n=='.')
+				this->spaces++;
+			// If we encounter \n, there must be a new line so height +1.
+			if (file_maze.peek()=='\n')
 				height++;
 		}
 		width/=height;
 		file_maze.close();
 		this->width=width;
 		this->height=height;
-		this->maze.resize(width*height);	// Define the size of the maze vector.
+		// Define the size of the maze vector.
+		this->maze.resize(width*height);
 	}
 	else std::cout<<"Error opening file"<<std::endl;
+	
 // Second part of the constructor: load the information of the maze.
 // Because we initially don't know what the size of the maze is, we first read the dimensions, then create a vector with the right dimension
 // and then read the file again to fill in the ones and zeroes (for walls and paths respectively) in the vector 'maze'.
@@ -33,13 +42,24 @@ Level::Level(std::string filename){
 				this->maze[i]=-1;
 			else if (n=='.')
 				this->maze[i]=0;
-		
 			i++;	
 		}
 		file_maze.close();
-	}else std::cout<<"Error opening file"<<std::endl;	
-}	
-
-
+	}else std::cout<<"Error opening file"<<std::endl;
 	
-	
+	// Generate numbers in maze with ratio to available spaces of 1/10
+	generate_numbers();
+}
+
+void Level::generate_numbers(){
+	srand(time(NULL));
+	// because this is an integer, it is rounded down
+	int nr_nr = this->spaces/10;
+	for (int i=0; i<nr_nr; i++){
+		int pos = rand()%(this->height*this->width);
+		while(this->maze[pos]!=0){
+			pos = rand()%(this->height*this->width);
+		}
+		this->maze[pos] = rand()%9+1;
+	}
+}
--- read.h
+++ read.h
@@ -5,13 +5,16 @@
 #include<string>
 #include<iostream>
 #include<fstream>
+#include <ctime>
 
 
 // declarations
 class Level
 {
-private:	
+private:
+	int spaces = 0;
 	std::string filename;
+	void generate_numbers();
 public:
 	Level(std::string filename);								// Constructor which has build-in dimensions.
 	int width, height;											//	Width and height of the maze.
