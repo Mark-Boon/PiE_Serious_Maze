24ddf7d1fecdd07abfebafd8a85c2508ca725ba0
DoveDoof <lennartknoll@live.nl>
2017-09-07 23:05:13 +0200

    Begin gemaakt aan startscherm

--- draw.h
+++ draw.h
@@ -8,9 +8,11 @@
 
 class Window {
 	int width, height;
+	std::vector<std::string> menu_items;
 public:
 	Window();	// resizes window
 	void draw_maze(int width, std::vector<int> &losmatrix, std::vector<int> &collected_numbers); // draws the level on screen (losmatrix)
+	void draw_title();
 };
 
 #endif
